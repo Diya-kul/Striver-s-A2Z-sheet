9eac5eac9c03aa9012ac198e76e182933ffb6aa8
Diya-kul <166923520+Diya-kul@users.noreply.github.com>
2026-03-11 23:10:02 +0530

    Create First_code.cpp

---
+++ First_code.cpp
@@ -0,0 +1,7 @@
+// The first step into the world of programming starts with a small program that says ‘Hello World’.
+# include <iostream>
+int main()
+{
+  std::cout << "Hello World";
+  return 0;
+}
